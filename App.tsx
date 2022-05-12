8f7a89e5048b206e469d1e7c6547bac1a96c3947
Alan Neri <alanneri10@gmail.com>
2022-05-12 11:40:52 -0500

    Add persistor pattern

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import {QueryClient, QueryClientProvider} from 'react-query';
+import {createAsyncStoragePersistor} from 'react-query/createAsyncStoragePersistor-experimental';
+import {persistQueryClient} from 'react-query/persistQueryClient-experimental';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import BankScreen from './src/screens/Bank.screen';
 
-const queryClient = new QueryClient();
+const HOURS_24 = 1000 * 60 * 60 * 24;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: HOURS_24,
+    },
+  },
+});
+
+const asyncStoragePersistor = createAsyncStoragePersistor({
+  storage: AsyncStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persistor: asyncStoragePersistor,
+});
 
 const App = () => {
   return (
